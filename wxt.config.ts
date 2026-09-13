4e06ceb35b0e159b1b1aac2e934463d29e5f1776
winkt0 <238452092+winkt0@users.noreply.github.com>
2026-09-13 15:23:47 +0200

    added icon + auto icon generation via @wxt-dev/auto-icons

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-    modules: ["@wxt-dev/module-react"],
+    modules: ["@wxt-dev/module-react", "@wxt-dev/auto-icons"],
+    autoIcons: {
+        enabled: true,
+        baseIconPath: "./assets/icon.svg",
+    },
     srcDir: "src",
     manifest: {
+        name: "annotabene",
+        description: "Annotate text, add notes, see a list of all your annotations in the extension's popup",
         permissions: ["contextMenus", "scripting", "storage", "activeTab"],
     },
 });
